7bfd77db4db8b6003533282fb4952807a1b1afe0
Marco Vanali <Marco91Vanali@gmail.com>
2017-10-09 20:40:06 +0100

    Modified the method 'input_students' it now asks for more information, puts a default value if no one is given, force the user to write the correct cohort. Created new mehod: 'correction_name' it allow you to correct the name of an enrolles student. Created new method: 'more_info_about_student', it allows you to know country, hobby and telephone number of an enrolled student.

--- directory.rb
+++ directory.rb
@@ -1,23 +1,64 @@
 # let's put all students into an array
 def input_students
-  puts "Please enter the names of the students"
-  puts "To finish, just hit return twice"
+  puts "Please enter all the information about the student you want to enrol."
+  puts "To finish, just hit return twice\n\n"
   # create an empty array
   students = []
   # get the first name
+  puts "Name:"
   name = gets.chomp
+  name = 'NotGiven' if name.empty?
   # while the name is not empty, repeat this code
   while !name.empty? do
+    # Asking more information about the student
+    puts "Cohort:"
+    months = [:January, :February, :March, :April, :May, :June, :July, :August, :September, :October, :November, :December]
+    cohort = gets.chomp.to_sym
+    while !months.include?(cohort)
+      puts "Please write the month correctly:"
+      cohort = gets.chomp.to_sym
+    end
+    puts "Hobby:"
+    hobby = gets.chomp
+    hobby = 'NotGiven' if hobby.empty?
+    puts "Country of birth:"
+    country = gets.chomp
+    country = 'NotGiven' if country.empty?
+    puts "Telephone number:"
+    telephone = gets.chomp
+    telephone = 'NotGiven' if telephone.empty?
+
+
     # add the student hash to the array
-    students << {name: name, cohort: :november}
-    puts "Now we have #{students.count} students"
+    students << {name: name, cohort: cohort, hobby: hobby, country: country, telephone: telephone }
+    puts "----------".center(50)
+    puts "Student enrolled. Now we have #{students.count} students".center(50)
     # get enother name from the user
+    puts "\nWrite the name of the next student you want to enrol:"
+    puts "To finish, just hit return.\n"
+
     name = gets.chomp
   end
   # return the array of students
   students
 end
 
+def correction_name(students, wrong_name, new_name)
+  students.each do |student|
+    if student[:name] == wrong_name
+      student[:name] = new_name
+      puts "Name of the student #{wrong_name} changed in #{new_name} \n"
+    end
+  end
+end
+
+def more_info_about_student(students, name)
+  students.each do |student|
+    if student[:name] == name
+      puts "#{name} was born in #{student[:country]}, its hobby is #{student[:hobby]} and its telephone number is #{student[:telephone]}."
+    end
+  end
+end
 
 def print_header
   puts "The students of Villains Academy".center(50)
@@ -62,3 +103,8 @@ print(students)
 print_names_starting_with("A", students)
 print_name_if_length_less_than(students, 12)
 print_footer(students)
+
+puts "   "
+more_info_about_student(students, "Marco")
+correction_name(students, 'Macro', 'Marco')
+print(students)
