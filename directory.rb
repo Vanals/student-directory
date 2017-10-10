b2590b07d774e56db3c422cfbb3ce80c0be546de
Marco Vanali <Marco91Vanali@gmail.com>
2017-10-10 17:44:09 +0100

    'month' local variable is now a global variable so can be used by more methods without repetitions. Added more descritive comments in the methods 'input_students'. The methods students_list_empty has been created, to check if the list is empty. It has been added in the following methods: 'correction_name', 'more_info_about_student', 'print_stud', 'print_by_cohort', print_names_starting_with', 'rpint_names_if_length_less_than. Created new method 'continue' to improve the user experience. It has been added in almost all the methods. Created an Interactive menu, now the user can chose what comand run. 'interactive_menu' run the comand print_menu , with contain the menu of the commands, and the 'process()' method which take care of the selection of the comand anfter that the user give the input. Plus other minor fix.

--- directory.rb
+++ directory.rb
@@ -1,21 +1,22 @@
-# let's put all students into an array
+# Create a global empty array, which will contain all the students information.
+$students = []
+$months = [:January, :February, :March, :April, :May, :June, :July, :August, :September, :October, :November, :December]
+
+# Let's put all students into an array
 def input_students
-  puts "Please enter all the information about the student you want to enrol."
+  puts "\nPlease enter all the information about the student you want to enrol.".upcase
   puts "To finish, just hit return twice\n\n"
-  # create an empty array
-  students = []
-  # get the name of the student.
+  # Get the name of the student.
   puts "Name:"
-  name = gets.chomp
+  name = gets.chomp.capitalize
   name = 'NotGiven' if name.empty?
 
-  # while the name of the next student is not empty, repeat this code
+  # While the name of the next student is not empty, repeat this code
   while !name.empty? do
     # Asking more information about the student(cohort, hobby, country of birth, telephone number)
     puts "\nCohort:"
-    months = [:January, :February, :March, :April, :May, :June, :July, :August, :September, :October, :November, :December]
     cohort = gets.chomp.to_sym
-    while !months.include?(cohort)
+    while !$months.include?(cohort)
       puts "Please write the month correctly:"
       cohort = gets.chomp.to_sym
     end
@@ -29,68 +30,81 @@ def input_students
     telephone = gets.chomp
     telephone = 'NotGiven' if telephone.empty?
 
-    # add the student hash to the array
-    students << {name: name, cohort: cohort, hobby: hobby, country: country, telephone: telephone }
+    # Add the student hash to the array
+    $students << {name: name, cohort: cohort, hobby: hobby, country: country, telephone: telephone }
     puts "----------".center(50)
+    # Change the value of 'singular_plural_stud' global variable, in base of how many students have been enrolled.
     $singular_plural_stud = 'students'
-    $singular_plural_stud = 'student' if students.count == 1
-    puts "Student enrolled. Now we have #{students.count} #{$singular_plural_stud}".center(50)
-    # get the name of the next student if chosen or finish the enrolment.
-    puts "\nWrite the name of the next student you want to enrol:"
+    $singular_plural_stud = 'student' if $students.count == 1
+    puts "Student enrolled. Now we have #{$students.count} #{$singular_plural_stud}".center(50).upcase
+    # Get the name of the next student if chosen or finish the enrolment.
+    puts "\nWrite the name of the next student you want to enrol:".upcase
     #If the user just pres return, without typing any name, the while loop will stop.
-    puts "To finish, just hit return.\n"
-    name = gets.chomp
+    puts "To finish and go back to the MENU, just hit return.\n"
+    name = gets.chomp.capitalize
   end
-  # return the array of students
-  students
+  # Return the array of students
+  $students
 end
 
-def correction_name(students, wrong_name, new_name)
-  students.each do |student|
+# This method allow us to correct the name of a student that has been enrolled.
+def correction_name
+  students_list_empty
+  #Going trough each hash, each student information.
+  puts "CORRECTION NAME\n"
+  puts "Which name do you want to correct?"
+  wrong_name = gets.chomp
+  puts "What is the new name?"
+  new_name = gets.chomp
+  $students.each do |student|
+    #If the name of a students is equal to what the user said, i will be replaced by the newone.
     if student[:name] == wrong_name
       student[:name] = new_name
-      puts "Name of the student #{wrong_name} changed in #{new_name} \n"
+      puts "\nName of the student #{wrong_name} changed in #{new_name} \n".upcase
+      continue
+      interactive_menu
     end
   end
 end
 
-def more_info_about_student(students, name)
-  students.each do |student|
+# This method give us more information about a student, like: telephone number, country of birth, hobby.
+def more_info_about_student
+  students_list_empty
+  puts "Who is the student you want to know more about?".upcase
+  name = gets.chomp
+  $students.each do |student|
     if student[:name] == name
-      puts "#{name} was born in #{student[:country]}, its hobby is #{student[:hobby]}"
+      puts "\n-#{name} was born in #{student[:country]}, its hobby is #{student[:hobby]}"
       puts "and its telephone number is #{student[:telephone]}."
+      continue
+      interactive_menu
     end
   end
 end
 
-def print_header
-  puts "The students of Villains Academy".center(50)
-  puts "--------------".center(50)
-end
-
-def print(students)
-  #I haven't added any if statement to check if the 'students' array is empty because there is no point doing that.
-  #If 'students' array is empty mean that 'input_students' method has not been used so the array 'students'
-  #has not been initialized and this method won't try to print an empty list, won't work.
+# 'print()' print out the list of the enrolled students.
+def print_stud
+  students_list_empty
   hash_index = 0
   list_position = 1
-  while hash_index < students.count
-    puts "#{list_position}. #{students[hash_index][:name]} (#{students[hash_index][:cohort]} cohort)\n"
+  while hash_index < $students.count
+    puts "#{list_position}. #{$students[hash_index][:name]} (#{$students[hash_index][:cohort]} cohort)\n"
     hash_index += 1
     list_position += 1
   end
 end
 
-def print_by_cohort(students)
-  #I haven't added any if statement to check if the 'students' array is empty because there is no point doing that.
-  #If 'students' array is empty mean that 'input_students' method has not been used so the array 'students'
-  #has not been initialized and this method won't try to print an empty list, won't work.
-  months = [:January, :February, :March, :April, :May, :June, :July, :August, :September, :October, :November, :December]
+# print_by_cohort() prints out the list of the enrolled students grouped by cohort.
+def print_by_cohort
+  students_list_empty
+
   months_index = 0
   list_position = 1
+  puts "The students of Villains Academy grouped by cohort".upcase.center(50)
+  puts "-------".center(50)
   while months_index < 12
-    students.each do |student|
-      if student[:cohort] == months[months_index]
+    $students.each do |student|
+      if student[:cohort] == $months[months_index]
         puts "#{list_position}. #{student[:name]} (#{student[:cohort]} cohort)\n"
         list_position += 1
       end
@@ -101,41 +115,107 @@ end
 
 # Thanks to the following method we can ask to the program to print only the students
 # which name starts with a specific letter.
-def print_names_starting_with(letter, students)
-  puts "-List of the students which name starts with the letter '#{letter}':\n"
-  students.each do |student|
-    puts student[:name] if student[:name][0] == letter
+def print_names_starting_with
+  students_list_empty
+  puts "Write the letter which the names of the students have to start with:"
+  letter = gets.chomp.upcase
+  puts "\nList of the students which name starts with the letter '#{letter}'\n".center(50, '-').upcase
+  list_position = 1
+  $students.each do |student|
+    if student[:name][0] == letter
+      puts "#{list_position}. #{student[:name]} (#{student[:cohort]} cohort)"
+      list_position += 1
+    end
   end
-  puts " "
+  continue
 end
 
 # Thanks to the following method we can ask to the program to print only the students
 # which name is shorter than a specific length.
-def print_name_if_length_less_than(students, length)
-  puts "-List of the students which name is shorter than #{length} characters:\n"
-    students.each do |student|
-      puts student[:name] if student[:name].length < length
+def print_name_if_length_less_than
+  students_list_empty
+  puts "The names have to be shorter than?"
+  length = gets.chomp
+  puts "\nList of the students which name is shorter than #{length} characters:\n".upcase.center(50)
+    $students.each do |student|
+      puts student[:name] if student[:name].length < length.to_i
     end
   puts " "
+  continue
 end
 
-def print_footer(names)
+private
+
+def print_header
+  puts "The students of Villains Academy".center(50).upcase
   puts "--------------".center(50)
-  puts "Overall, we have #{names.count} great #{$singular_plural_stud}".center(50)
 end
 
-#TEST COMMANDS ----------- TEST COMMANDS
-students = input_students
-print_header
-print(students)
-print_names_starting_with("A", students)
-print_name_if_length_less_than(students, 12)
-print_footer(students)
+def print_footer
+  puts "--------------".center(50)
+  puts "Overall, we have #{$students.count} great #{$singular_plural_stud}\n".center(50)
+end
 
-puts "   "
-more_info_about_student(students, "Marco")
-correction_name(students, 'Macro', 'Marco')
-print(students)
-puts " "
-puts "List printed by order"
-print_by_cohort(students)
+def continue
+  puts "\nTo continue and go back to the MENU, press enter."
+  gets
+end
+
+def students_list_empty
+  if $students.empty?
+    puts "The list of students is empty".upcase
+    continue
+    interactive_menu
+  end
+end
+
+def print_menu
+  puts "COMANDS MENU OF VILLAINS ACADEMY".center(50, '-')
+  puts "by @Vanals directory Company".center(50)
+  puts " "
+  puts "Choose the corrisponding number to the command, write it and then press enter for doing what you desire:\n \n".center(50)
+  puts "0. EXIT = Interrupt the program"
+  puts "1. SIGN UP = Enrols new students in the Villains Accademy"
+  puts "2.CORRECTION NAME = Changes the name of an enrolled student."
+  puts "3.INFO ABOUT = Prints into the screen information about a student of your choice."
+  puts "4.PRINT LIST = Prints into the screen the list of all the enrolled students."
+  puts "5.PRINT LIST BY COHORT = Prints into the screen the list of all the enrolled students grouped by cohort."
+  puts "6.PRINT STUDENTS NAME STARTING WITH = Prints into the screen the list of all the enrolled students which name start with a letter of your choice.'"
+  puts "7.PRINT STUDENTS NAME LESS LENGTH THAN = Print into the screen the list of all the enrolled students wich name is shorter than an amount of your choice."
+  puts "\nWrite the corrisponding number and then press enter:"
+  puts "> "
+end
+
+def process(selection)
+  case selection
+    when "0" then exit(0)
+    when "1" then input_students
+    when "2" then correction_name
+    when "3" then more_info_about_student
+    when "4"
+      print_header
+      print_stud
+      print_footer
+      continue
+    when "5"
+      print_by_cohort
+      print_footer
+      continue
+    when "6" then print_names_starting_with
+    when "7" then print_name_if_length_less_than
+    else
+      puts "This command doesn't exist. Write the command correctly. Press enter to go back to the MENU"
+      continue
+  end
+end
+
+def interactive_menu
+  loop do
+    print_menu
+    user_comand = gets.chomp
+    process(user_comand)
+  end
+end
+
+
+interactive_menu
